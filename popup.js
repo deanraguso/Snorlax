51a3778496fdf36c18edba03a49bd0cc4d03b5fe
deanraguso <deanraguso@gmail.com>
2022-07-08 13:43:41 +1000

    fix: move script tag to bottom of html

--- popup.js
+++ popup.js
@@ -1,17 +1,13 @@
-(window.onload = function () {
-  const snorlax = document.getElementById("Snorlax");
-  const updateKeys = document.getElementById("UpdateKeys");
+const snorlax = document.getElementById("Snorlax");
+const updateKeys = document.getElementById("UpdateKeys");
 
-  if (snorlax && updateKeys) {
-    const handleSnorlax = () => {
-      alert("Yawn!!");
-    };
+const handleSnorlax = () => {
+  alert("Yawn!!");
+};
 
-    const handleUpdateKeys = () => {
-      alert("Handle update keys!!!");
-    };
+const handleUpdateKeys = () => {
+  alert("Handle update keys!!!");
+};
 
-    snorlax.addEventListener("click", handleSnorlax);
-    updateKeys.addEventListener("click", handleUpdateKeys)
-  }
-})();
+snorlax.addEventListener("click", handleSnorlax);
+updateKeys.addEventListener("click", handleUpdateKeys);
