c7fd0e92bd96a10da076988a1b14708a53b6e13f
deanraguso <deanraguso@gmail.com>
2022-07-08 14:05:20 +1000

    feat: add key persistence

--- background.js
+++ background.js
@@ -1,12 +1,6 @@
 let snorlaxState = true;
 let snorlaxKeys = "carpet,oversized,danger"
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ snorlaxState, snorlaxKeys });
-});
-
-const getKeys = (callback) => {
-  chrome.storage.sync.get(["snorlaxKeys"], ({ snorlaxKeys }) => callback(snorlaxKeys))
-}
-
-getKeys(console.log)
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ snorlaxState, snorlaxKeys });
+// });
--- popup.js
+++ popup.js
@@ -1,13 +1,21 @@
 const snorlax = document.getElementById("Snorlax");
 const updateKeys = document.getElementById("UpdateKeys");
+const keyInput = document.getElementById("KeyInput");
 
 const handleSnorlax = () => {
   alert("Yawn!!");
 };
 
-const handleUpdateKeys = () => {
-  alert("Handle update keys!!!");
+const saveKeys = (snorlaxKeys) => {
+  console.log(`Saving Keys: ${snorlaxKeys}`);
+  chrome.storage.sync.set({ snorlaxKeys });
 };
 
+chrome.storage.sync.get(["snorlaxKeys"], ({ snorlaxKeys }) => {
+  keyInput.value = snorlaxKeys;
+});
+
 snorlax.addEventListener("click", handleSnorlax);
-updateKeys.addEventListener("click", handleUpdateKeys);
+updateKeys.addEventListener("click", () => {
+  saveKeys(keyInput.value);
+});
