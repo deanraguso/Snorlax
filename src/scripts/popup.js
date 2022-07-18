59ba5422ad5da213a09e975001f10681e17192b5
Dean Raguso <deanraguso@gmail.com>
2022-07-18 22:21:18 +1000

    feat: add easter egg

--- src/scripts/popup.js
+++ src/scripts/popup.js
@@ -1,9 +1,15 @@
 const snorlax = document.getElementById("Snorlax");
+const title = document.getElementById("Title");
 const updateKeys = document.getElementById("UpdateKeys");
 const keyInput = document.getElementById("KeyInput");
 
 const handleSnorlax = () => {
   alert("Yawn!!");
+};
+
+const handleEasterEgg = () => {
+  snorlax.src = "/src/assets/Terasa2D.png";
+  title.innerText = "Terasa"
 };
 
 const saveKeys = (snorlaxKeys) => {
@@ -15,6 +21,7 @@ chrome.storage.sync.get(["snorlaxKeys"], ({ snorlaxKeys }) => {
   keyInput.value = snorlaxKeys;
 });
 
+snorlax.addEventListener("wheel", handleEasterEgg);
 snorlax.addEventListener("click", handleSnorlax);
 updateKeys.addEventListener("click", () => {
   saveKeys(keyInput.value);
